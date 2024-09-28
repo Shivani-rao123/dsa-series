092cb085496aeb39c384b37351f2fcd7555f5da7
ShivaniBS <22c19.shivanib@sjec.ac.in>
2024-09-28 19:08:08 +0530

    for loop in cpp

---
+++ forloop.cpp
@@ -0,0 +1,13 @@
+//for(initialization;condition;increament)
+//work
+#include<iostream>
+using namespace std;
+int main(){
+    int sum=0;
+    for(int i=1;i<=5;i++){
+        sum=sum+i;
+        
+    }
+    cout<<"The sum of the first 5 natural numbers is: "<<endl;
+    cout<<sum<<endl;
+}
---
+++ whileloops.cpp
@@ -0,0 +1,17 @@
+//while loop
+#include<iostream>
+using namespace std;
+int main(){
+    int i=0;
+    int n;
+    cout<<"enter the number:";
+    cin>>n;
+    while(i<n){
+        
+        cout<<i<<endl;
+        i++;
+    }
+    return 0;
+
+}//not to write the infinite loop
+//fills with the memory
