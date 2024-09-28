848d25630eff0d593d8fb37a6d613fc45e361e57
ShivaniBS <22c19.shivanib@sjec.ac.in>
2024-09-28 22:46:55 +0530

    nested loops

---
+++ primeornot.cpp
@@ -0,0 +1,35 @@
+// #include<iostream>
+// using namespace std;
+// int main()
+// {
+//     int n=11;
+//     bool isPrime=true;
+//     for(int i=2;i*i<=n;i++){
+//         if(n%i==0){
+//             isPrime=false;
+//             break;
+//         }
+//     }
+//     if(isPrime==true){
+//         cout<<"Prime Number"<<endl;
+//     }
+//     else{
+//         cout<<"Not a Prime Number"<<endl;
+//     }
+//     return 0;
+// }
+//factors are unique from 1 to root n
+//Nested loops
+#include<iostream>
+using namespace std;
+int main(){
+    int n=10;
+  for(int i=1;i<=n;i++){//how many lines should be ther
+    int m=5;
+    for(int i=1;i<=m;i++){// how many stars should be there
+        cout<<"*";
+    }
+    cout<<endl;
+  }
+    cout<<endl;
+}
