3c95ed2ea06bac788a96448aa3683a915182aaf1
ShivaniBS <22c19.shivanib@sjec.ac.in>
2024-09-28 19:23:27 +0530

    while stateement

--- forloop.cpp
+++ forloop.cpp
@@ -1,14 +1,32 @@
-// for(initialization;condition;increament)
-// work
+// // for(initialization;condition;increament)
+// // work
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int sum=0;
+//     for(int i=1;i<=5;i++){
+//         sum=sum+i;
+        
+//     }
+//     cout<<"The sum of the first 5 natural numbers is: "<<endl;
+//     cout<<sum<<endl;
+//     return 0;
+// }
 #include<iostream>
 using namespace std;
 int main(){
+    int n;
+    int i=1;
     int sum=0;
-    for(int i=1;i<=5;i++){
-        sum=sum+i;
-        
-    }
-    cout<<"The sum of the first 5 natural numbers is: "<<endl;
-    cout<<sum<<endl;
-    return 0;
+    cout<<"Enter the number: ";
+    cin>>n;
+   while(n>=i){
+         sum=sum+i;
+         i++;
+   }
+   cout<<"The sum of the first "<<n<<" natural numbers is: "<<sum<<endl;
+   return 0;
 }
+//break statement we will come out of the loop
+
+
