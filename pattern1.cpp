a2cdaa19b525205fe13e016609b00623a7b86591
ShivaniBS <22c19.shivanib@sjec.ac.in>
2024-09-29 15:04:09 +0530

    trianle pattern

--- pattern1.cpp
+++ pattern1.cpp
@@ -1,22 +1,52 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+   
+//     int m,i,j;
+//     int n;
+    
+//     cout<<"Enter the number of rows"<<endl;
+//     cin>>n;
+//     char ch='A';
+    
+//     for(i=0;i<n;i++){
+        
+//         for(j=0;j<n;j++){
+//             cout<<ch<<" ";
+//             ch++;
+//         }
+//         cout<<endl;
+// }
+
+//     return 0;
+// }
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int n=4;
+//     int i,j;
+//     for(i=0;i<n;i++){
+//         for(j=0;j<=i;j++){
+//             cout<<"*";
+            
+//         }
+//         cout<<endl;
+       
+//     }
+// }
+
 #include<iostream>
 using namespace std;
 int main(){
-   
-    int m,i,j;
-    int n;
-    
-    cout<<"Enter the number of rows"<<endl;
-    cin>>n;
-    char ch='A';
-    
+    int i,j;
+    int n=4;
     for(i=0;i<n;i++){
-        
-        for(j=0;j<n;j++){
-            cout<<ch<<" ";
-            ch++;
+        for(j=0;j<=i;j++){
+            cout<<i+1;
         }
         cout<<endl;
+
+    }
+
 }
 
-    return 0;
-}
