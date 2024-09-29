a258f386ccb1d51c76434c1b89e060a8b8a59b2a
ShivaniBS <22c19.shivanib@sjec.ac.in>
2024-09-29 15:30:48 +0530

    more on patterns

--- pattern1.cpp
+++ pattern1.cpp
@@ -35,18 +35,66 @@
 //     }
 // }
 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i,j;
+//     int n=4;
+//     for(i=0;i<n;i++){
+//         for(j=0;j<=i;j++){
+//             cout<<i+1;
+//         }
+//         cout<<endl;
+
+//     }
+
+// }
+
+//for charcters 
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int i,j;
+//     int n=4;
+//     char ch='A';
+
+//     for(i=0;i<n;i++){
+//         for(j=0;j<=i;j++){
+//             cout<<ch;
+            
+//         }
+//         ch++;
+//         cout<<endl;
+       
+//         }
+//         return 0;
+
+//     }
+
+//numbers
 #include<iostream>
 using namespace std;
 int main(){
     int i,j;
     int n=4;
+   
+   
+
     for(i=0;i<n;i++){
-        for(j=0;j<=i;j++){
-            cout<<i+1;
+        for(j=1;j<=i+1;j++){
+            cout<<j;
+            
+            
         }
+        j+1;
         cout<<endl;
+       
+        }
+        return 0;
 
     }
 
-}
+
+
+
 
---
+++ reversepattern.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    int i,j;
+    for(i=0;i<n;i++){
+     for(j=i+1;j>0;j--){
+        cout<<j<<" ";
+     }
+     cout<<endl;
+    }
+
+}
