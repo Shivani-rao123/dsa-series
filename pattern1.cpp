8719c731a69f6000ce84ec03bef0cf96875e1c19
ShivaniBS <22c19.shivanib@sjec.ac.in>
2024-09-30 21:17:45 +0530

    learnt

--- pattern1.cpp
+++ pattern1.cpp
@@ -2,11 +2,12 @@
 // using namespace std;
 // int main(){
    
-//     int m,i,j;
+//     int i,j;
 //     int n;
     
 //     cout<<"Enter the number of rows"<<endl;
 //     cin>>n;
+
 //     char ch='A';
     
 //     for(i=0;i<n;i++){
--- patterns.cpp
+++ patterns.cpp
@@ -7,7 +7,7 @@ int main(){
     for(int i=0;i<n;i++){
         char ch='A';//next line it must start with A thwats why it is inside one for loop
         for(int j=0;j<=n;j++){
-            cout<<ch;
+            cout<<ch<<endl;
             ch=ch+1;
 
         }
