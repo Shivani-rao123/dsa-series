124af4f2457ebef3920ce86e6d2cec28d4e867fe
ShivaniBS <22c19.shivanib@sjec.ac.in>
2024-10-05 23:15:03 +0530

    hollow triangle made correct

--- hollowdiamind.cpp
+++ hollowdiamind.cpp
@@ -1,39 +1,43 @@
 #include<iostream>
 using namespace std;
-int main(){
-    int n=4;
-    //we are breaking into top and bottom part
-    //toppart
-    for(int i=0;i<n;i++)
-    {
-        //spaces will be n-i-1
-        for(int j=0;j<n-i-1;j++){
-            cout<<" ";
 
-        }
-        cout<<"*";
-        //in between space will be odd numbers given as 2*i-1
-        if (i!=0){
-            for(int j=0;j<2*i-1;j++){
-                cout<<" ";
-            }
-            cout<<"*";
-        }
-        cout<<endl;
-        //bottom part
-        for(int i=0;i<n-1;i++){
-            for(int j=0;j<i+1;j++){
-                cout<<" ";
-            }
-            cout<<"*";
+int main() {
+    int n = 4;
+    // We are breaking into top and bottom parts
 
+    // Top part
+    for (int i = 0; i < n; i++) {
+        // Spaces will be n-i-1
+        for (int j = 0; j < n - i - 1; j++) {
+            cout << " ";
         }
-        if(i!=n-2){
-            for(int j=0;j<2*(n-i)-5;j++){
-                cout<<" ";
+        cout << "*";
+
+        // In between spaces will be odd numbers given as 2*i-1
+        if (i != 0) {
+            for (int j = 0; j < 2 * i - 1; j++) {
+                cout << " ";
             }
-            cout<<"*";
+            cout << "*";
         }
-        cout<<endl;
+        cout << endl;
     }
+
+    // Bottom part
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < i + 1; j++) {
+            cout << " ";
+        }
+        cout << "*";
+
+        if (i != n - 2) {
+            for (int j = 0; j < 2 * (n - i) - 5; j++) {
+                cout << " ";
+            }
+            cout << "*";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }
