ad92f3ce733d1c75135b575befe325bb8c9dc2cf
ShivaniBS <22c19.shivanib@sjec.ac.in>
2024-10-05 23:03:51 +0530

    hollow diamond which is not correst

---
+++ hollowdiamind.cpp
@@ -0,0 +1,39 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n=4;
+    //we are breaking into top and bottom part
+    //toppart
+    for(int i=0;i<n;i++)
+    {
+        //spaces will be n-i-1
+        for(int j=0;j<n-i-1;j++){
+            cout<<" ";
+
+        }
+        cout<<"*";
+        //in between space will be odd numbers given as 2*i-1
+        if (i!=0){
+            for(int j=0;j<2*i-1;j++){
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+        cout<<endl;
+        //bottom part
+        for(int i=0;i<n-1;i++){
+            for(int j=0;j<i+1;j++){
+                cout<<" ";
+            }
+            cout<<"*";
+
+        }
+        if(i!=n-2){
+            for(int j=0;j<2*(n-i)-5;j++){
+                cout<<" ";
+            }
+            cout<<"*";
+        }
+        cout<<endl;
+    }
+}
--- invertedtrianle.cpp
+++ invertedtrianle.cpp
@@ -1,3 +1,52 @@
+// #include<iostream>
+// using namespace std;
+// int main(){//first there is some space and then there is a number
+// //first number does not have space
+//     //in the second line there is one space
+//     //in the third line there are two spaces
+//     // there are i number of spaces
+    
+// for(int i=0;i<4;i++){
+//     for(int j=0;j<i;j++){
+//         cout<<" ";
+
+//     }// we have printed spaces first
+//     for(int j=0;j<4-i;j++){
+//         cout<<i+1;
+//     }// number  will be printed 4-i times and i+1 number is printed
+
+//     cout<<endl;
+// }
+// }
+
+// #include<iostream>
+// using namespace std;
+// int main(){//first there is some space and then there is a number
+// //first number does not have space
+//     //in the second line there is one space
+//     //in the third line there are two spaces
+//     // there are i number of spaces
+    
+// for(int i=0;i<4;i++){
+//     for(int j=0;j<i;j++){
+//         cout<<" ";
+
+//     }// we have printed spaces first
+//     for(int j=0;j<4-i;j++){
+//         cout<<i+1<<" ";
+//     }// number  will be printed 4-i times and i+1 number is printed
+
+//     cout<<endl;
+// }
+// }
+
+//straight pyramid
+   // 1
+//  1 2 1
+// 1 2 3 2 1
+//1 2 3 4 3 2 1
+//it will be n-i-1 spaces
+
 #include<iostream>
 using namespace std;
 int main(){//first there is some space and then there is a number
@@ -7,14 +56,22 @@ int main(){//first there is some space and then there is a number
     // there are i number of spaces
     
 for(int i=0;i<4;i++){
-    for(int j=0;j<i;j++){
+    
+    // spaces:n-i-1
+    for(int j=0;j<4-i-1;j++){
         cout<<" ";
 
     }// we have printed spaces first
-    for(int j=0;j<4-i;j++){
-        cout<<i+1;
-    }// number  will be printed 4-i times and i+1 number is printed
-    
+
+    //number set 1:i+1
+    for (int j=1;j<=i+1;j++){
+        cout<<j;
+    }
+    //number set 2
+    for(int j=i;j>0;j--){
+        cout<<j;
+
+    }
     cout<<endl;
 }
 }
