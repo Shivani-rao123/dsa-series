8635f38bb859c36b9ae77ae600025dc014b0bccf
ShivaniBS <22c19.shivanib@sjec.ac.in>
2024-10-13 21:31:15 +0530

    .......

--- functions/function.cpp
+++ functions/function.cpp
@@ -12,13 +12,27 @@
 //         return 0;
 //     }
 //sum of two numbers
+// #include<iostream>
+// using namespace std;
+// int sum( int a,int b){
+//     int s=a +b;
+//     return s;
+// }
+// int main(){
+//     cout<<sum(10,5)<<endl;
+//     return 0;
+//}//stack and heap memory heap for dynamic allocation and the stack for static allocation
+//return must be always at the end
+//pass by value:
 #include<iostream>
 using namespace std;
-int sum( int a,int b){
-    int s=a +b;
-    return s;
+int sun(int x,int y){
+    int sum=y+x;
+    return sun();
 }
 int main(){
-    cout<<sum(10,5)<<endl;
+    int a=10,b=20;
+    cout<<sun(a,b)<<endl;
     return 0;
 }
+
