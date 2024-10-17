3d296e6359faed493cf60d6f33bd7bf8695601f2
ShivaniBS <22c19.shivanib@sjec.ac.in>
2024-10-17 18:46:54 +0530

    .....

---
+++ Arrays.cpp
@@ -0,0 +1,33 @@
+//Data structures:Arrays
+//only same type of data can be stored
+//arrays are contigous in memory
+//they are also linear data structures
+//int marks[5]
+//accessing the array we use array index
+//arrays inside the curly braces
+
+// #include<iostream>
+// using namespace std;
+
+// int main(){
+//    int marks[5]={99,100,54,36,88};
+  
+  
+//     cout<<marks[0]<<endl;
+//     cout<<marks[1]<<endl;
+
+//    return 0;
+   
+// }
+
+//loop in arrays
+
+#include<iostream>
+using namespace std;
+int main(){
+    int marks[5]={99,100,54,36,88};
+    int size=5;
+    //int sz=sizeof(marks)/
+    
+    return 0;
+}
