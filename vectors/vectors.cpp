edbef8dcb2caa4b2828f70b0620a2b0bfa86a30d
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-03-03 23:16:57 +0530

    ...

--- vectors/vectors.cpp
+++ vectors/vectors.cpp
@@ -1,14 +1,37 @@
+// #include <iostream>
+// #include <vector>
+// using namespace std;
+
+// int main() {
+//     vector<int> vec;
+//      cout<<"size="<<vec.size()<<endl;
+//      vec.push_back(25);
+//      vec.push_back(35);
+//      vec.push_back(45);
+//      cout<<"after push back"<<vec.size()<<endl;
+//      vec.pop_back();//45 is deleted
+//     for (int  val : vec)
+//     {
+//         cout << val << endl;
+//     }
+
+//     return 0;
+// }
+
 #include <iostream>
 #include <vector>
 using namespace std;
 
 int main() {
-    vector<char> vec = {'a', 'b', 'c', 'd', 'e'};
-
-    for (char val : vec)
-    {
-        cout << val << endl;
-    }
-
+    vector<int> vec;
+     cout<<"size="<<vec.size()<<endl;
+     vec.push_back(25);
+     vec.push_back(35);
+     vec.push_back(45);
+     cout<<"after push back"<<vec.size()<<endl;
+     vec.pop_back();//45 is deleted
+    cout<< vec.front()<<endl;
+    cout<<vec.back()<<endl;
+    cout<<vec.at[2]<<endl;
     return 0;
 }
