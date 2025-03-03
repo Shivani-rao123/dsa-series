c6dfcdabd2ddc9c2a34eb3138832247e87fad1cb
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-03-04 00:04:38 +0530

    ....

--- vectors/vectors.cpp
+++ vectors/vectors.cpp
@@ -18,20 +18,34 @@
 //     return 0;
 // }
 
-#include <iostream>
-#include <vector>
-using namespace std;
+// #include <iostream>
+// #include <vector>
+// using namespace std;
 
-int main() {
-    vector<int> vec;
-     cout<<"size="<<vec.size()<<endl;
-     vec.push_back(25);
-     vec.push_back(35);
-     vec.push_back(45);
-     cout<<"after push back"<<vec.size()<<endl;
-     vec.pop_back();//45 is deleted
-    cout<< vec.front()<<endl;
-    cout<<vec.back()<<endl;
-    cout<<vec.at[2]<<endl;
-    return 0;
+// int main() {
+//     vector<int> vec;
+//      cout<<"size="<<vec.size()<<endl;
+//      vec.push_back(25);
+//      vec.push_back(35);
+//      vec.push_back(45);
+//      cout<<"after push back"<<vec.size()<<endl;
+//      vec.pop_back();//45 is deleted
+//     cout<< vec.front()<<endl;
+//     cout<<vec.back()<<endl;
+//     cout<<vec.at[2]<<endl;
+//     return 0;
+// }
+
+#include<iostream>
+#include<vector>
+using namespace std;
+int main(){
+    vector<int>vec;
+    vec.push_back(0);
+    vec.push_back(1);
+    vec.push_back(2);
+    vec.push_back(3);
+    vec.push_back(4);
+    cout<<vec.size()<<endl;
+    cout<<vec.capacity()<<endl;
 }
