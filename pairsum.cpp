151b96fcb63db92a48554837193975a182d6424c
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-05-29 22:17:16 +0530

    second approach added for the pairsum

--- pairsum.cpp
+++ pairsum.cpp
@@ -1,20 +1,56 @@
+//this is the first aproach
+
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+// vector<int> twoSum(vector<int>vec, int target) {
+//     vector<int>ans;
+//     int n=vec.size();
+//     for(int i =0;i<n;i++){
+//         for(int j=i+1;j<n;j++){
+//            if(vec[i]+vec[j]==target){
+//             ans.push_back(i);
+//             ans.push_back(j);
+//             return ans;
+
+//            }
+//         }
+//     }
+//     return ans;
+// }
+
+// int main(){
+//     vector<int>vec={2,7,11,15};
+//     int target=9;
+//     vector<int>ans =twoSum(vec,target);
+//     cout<<ans[0]<<" "<<ans[1]<<endl;
+//     return 0;
+
+// }
+
+//second approach
+
 #include<iostream>
 #include<vector>
 using namespace std;
 vector<int> twoSum(vector<int>vec, int target) {
     vector<int>ans;
     int n=vec.size();
-    for(int i =0;i<n;i++){
-        for(int j=i+1;j<n;j++){
-           if(vec[i]+vec[j]==target){
+    int i=0,j=n-1;
+    while(i<j){
+        int pairsum=vec[i]+vec[j];
+        if(pairsum>target)
+            j--;
+        else if(pairsum<target)
+            i++;
+        else{
             ans.push_back(i);
             ans.push_back(j);
             return ans;
-
-           }
         }
     }
     return ans;
+  
 }
 
 int main(){
