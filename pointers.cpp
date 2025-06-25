4f62f677d4b49c776717d9fc18f0c316bf2e72e3
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-06-25 19:30:34 +0530

    null value about the pointer

--- pointers.cpp
+++ pointers.cpp
@@ -1,19 +1,29 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int main(){
+//     int a=10;
+//     int * ptr=&a;//ptr will store the adress of a 
+    
+//     int**parPtr =&ptr;// parPtr will store the address of ptr which is address of a
+
+//     cout<<*(parPtr)<<endl;
+    
+//    cout<<&ptr<<endl;
+//    cout<<parPtr<<endl;
+//    cout<<*(&a)<<endl;
+//    cout<<*(ptr)<<endl;
+
+//     return 0;
+// }
+
 #include<iostream>
 #include<vector>
 using namespace std;
 
 int main(){
-    int a=10;
-    int * ptr=&a;//ptr will store the adress of a 
-    
-    int**parPtr =&ptr;// parPtr will store the address of ptr which is address of a
-
-    cout<<*(parPtr)<<endl;
-    
-   cout<<&ptr<<endl;
-   cout<<parPtr<<endl;
-   cout<<*(&a)<<endl;
-   cout<<*(ptr)<<endl;
-
-    return 0;
+   int** ptr=NULL;
+   cout<<ptr<<endl; 
+   return 0;
 }
