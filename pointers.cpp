586999b8beed75ba256e237be6af654d840a8ac0
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-06-25 18:56:48 +0530

    dereferncing pointer

--- pointers.cpp
+++ pointers.cpp
@@ -4,10 +4,16 @@ using namespace std;
 
 int main(){
     int a=10;
-    int * ptr=&a;
-    cout<<ptr<<endl;
-    cout<<&a<<endl;
-    cout<<*ptr<<endl;
+    int * ptr=&a;//ptr will store the adress of a 
+    
+    int**parPtr =&ptr;// parPtr will store the address of ptr which is address of a
+
+    cout<<*(parPtr)<<endl;
+    
+   cout<<&ptr<<endl;
+   cout<<parPtr<<endl;
+   cout<<*(&a)<<endl;
+   cout<<*(ptr)<<endl;
 
     return 0;
 }
