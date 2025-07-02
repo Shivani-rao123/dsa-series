a343ec97ce099717c9c34b09969dc4afb3856d1b
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-07-02 22:12:51 +0530

    book allocation problem

---
+++ bookallocation.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+bool isValid(vector<int> arr, int n, int m, int maxAllowedPages) { // O(n)
+    int stu = 1, pages = 0;  // initialize stu = 1
+    for (int i = 0; i < n; i++) {
+        if (arr[i] > maxAllowedPages) {
+            return false;
+        }
+        if (pages + arr[i] <= maxAllowedPages) {
+            pages += arr[i];
+        } else {
+            stu++;
+            pages = arr[i];
+        }
+    }
+    return stu <= m;
+}
+
+int allocatebooks(vector<int> arr, int n, int m) {
+    if (m > n)
+        return -1;  // edge case
+
+    int sum = 0;
+    for (int i = 0; i < n; i++) {
+        sum += arr[i];
+    } // finding the sum of the array
+
+    int start = 0, end = sum;
+    int ans = -1;
+
+    while (start <= end) { // O(log(Range)) = O(log(n)*n)
+        int mid = start + (end - start) / 2;
+        if (isValid(arr, n, m, mid)) {
+            ans = mid;
+            end = mid - 1;
+        } else {
+            start = mid + 1;
+        }
+    }
+    return ans;
+}
+
+int main() {
+    vector<int> arr = {2, 1, 3, 4};
+    int n = 4, m = 2;
+    cout << allocatebooks(arr, n, m) << endl;
+    return 0;
+}
