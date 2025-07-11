e4f602b051bbc23734bec43bc6d716e998590731
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-07-11 11:35:55 +0530

    vector pait difference between pushback and emplaceback

---
+++ vectorpair.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include<vector>
+#include<list>
+#include<deque>
+using namespace std;
+int main() {
+    vector<pair<int,int>> vec={{ 1,2 },{2,3},{3,4}};
+    vec.push_back({ 4,5 });//insert
+    vec.emplace_back(4,5);//in-place objects create
+    for(auto p: vec){
+        cout<< p.first <<" "<<p.second<<endl;
+    }
+    return 0;
+}
