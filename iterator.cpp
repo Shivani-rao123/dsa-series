9fa63a0e843d4a6673adbea1073c98876a14edf7
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-07-11 10:54:51 +0530

    iterator

---
+++ iterator.cpp
@@ -0,0 +1,31 @@
+// #include<iostream>
+// #include<vector>
+// using namespace std;
+
+// int main(){
+//     vector <int> vec = {1, 2, 3, 4, 5};
+//     vector<int>::iterator it;
+//     for(it=vec.begin();it!=vec.end();it++){
+//         cout<<*(it)<<" "; // Output: 1 2 3 4 5
+//     }
+//     cout<<endl;
+//     return 0;
+// }// fprward loop
+
+
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(){
+    vector <int> vec = {1, 2, 3, 4, 5};
+    vector<int>::reverse_iterator it;
+    for(it=vec.rbegin();it!=vec.rend();it++){
+        cout<<*(it)<<" "; // Output: 1 2 3 4 5
+    }
+    cout<<endl;
+    return 0;
+}// backward loop
+
+
+    
