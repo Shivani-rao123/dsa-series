1d28e0700adca4d841959ee739475314a24bedd6
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-07-27 22:50:18 +0530

    linear search with pair

--- linearsearch2d.cpp
+++ linearsearch2d.cpp
@@ -1,18 +1,21 @@
 #include<iostream>
 using namespace std;
-bool linearSearch(int matrix[][3], int rows, int cols,int target){
+pair<int,int> linearSearch(int matrix[][3], int rows, int cols,int target){
+    
     for(int i=0;i<rows;i++){
         for(int j=0;j<cols;j++){
             if(matrix[i][j]== target){
-                return true;
+                return {i,j};
             }
         }
     }
-    return false;
+    return {-1,-1};
+   
 }
 
 
 int main() {
+    
     int matrix[4][3] = {
         {1, 2, 3},
         {4, 5, 6},
@@ -22,8 +25,10 @@ int main() {
     int target=9;
     int rows = 4;
     int cols = 3;
-    if(linearSearch(matrix, rows, cols, target)){
-        cout << "Element found in the matrix." << endl;
+    pair<int,int> p = linearSearch(matrix, rows, cols, target);
+
+    if(p.first !=-1){
+        cout << "Element found in the matrix." <<p.first<<","<<p.second<< endl;
     } else {
         cout << "Element not found in the matrix." << endl;
     }
