ce6087a4c32c336ef5c243c998a48cd610ee4083
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-07-31 10:46:51 +0530

    recursion

--- recursio.cpp
+++ recursio.cpp
@@ -1,11 +1,17 @@
 #include<iostream>
 using namespace std;
 
-void abc() {
-     abc();
+void printNums(int n) {
+    if(n==1){
+        cout<<"1\n";
+        return;
+    }
+     cout<< n<<" ";//n-1,n-2 ...1
+     printNums(n-1);
 
 }
 int main() {
+    printNums(4);
 
     return 0;
 }
