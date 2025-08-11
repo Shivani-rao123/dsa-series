549bd81b7daad4cd86d0ab04743c3f5a32e4d83d
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-08-11 18:42:08 +0530

    polymorphism

---
+++ oops/polymorphism.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+#include<string>
+using namespace std;
+class Person{
+    public:
+       name string;
+       Student(){
+        cout<<"Non-parameterized\n"
+       }
+       Student(string name){
+        this-> name=name
+        cout<<"parameterized\n";
+       }
+      
+
+};
+int main() {
+
+  Student s1;
+
+    return 0;
+}
