a76f1283d002a0fe779b75f6291a7cf3f13dea1b
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-08-20 00:03:57 +0530

    pushback thing

--- linkedlist.cpp
+++ linkedlist.cpp
@@ -20,7 +20,24 @@ public:
   List() {
     head = tail =NULL;
   }
+  void push_front(int val)
+{
+    Node* newNode =new Node(val);//dynamic
+    if(head ==NULL){
+        head = tail = newNode;
+        return;
+    }
+    else{
+        newNode ->next =head;
+        head =newNode;
+    }
+   
+}
 };
 int main() {
+    List ll;
+    ll.push_front(1);
+    ll.push_front(2);
+    ll.push_front(3);
     return 0;
 }
