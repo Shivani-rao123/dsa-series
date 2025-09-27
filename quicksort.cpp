b3e4d959bc8049c5d2533897b9fd14768461880c
ShivaniBS <22c19.shivanib@sjec.ac.in>
2025-09-27 12:21:56 +0530

    binary trees and graphs

---
+++ quicksort.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+void quickSort(vector<int>&arr,int st,int end){
+    if(st<end){
+        int pivot()
+    }
+
+}
+int main(){
+    vector<int>arr={12,31,35,8,32,17}
+    quickSort(arr,0,arr.size()-1);
+
+    return 0;
+}
